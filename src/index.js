6af1560d02ad38b4ed9a8655e42269486f26dbef
wizardywizard <littledavid175@gmail.com>
2020-02-10 13:04:29 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImage() {
+     fetch("https://dog.ceo/api/breeds/image/random/4")
+     .then(function(response) {
+       return response.json();
+     })
+     .then(function(json) {
+       console.log(json)
+     });
+   }
+
+//    function renderImage(json) {
+//     const main = document.querySelector('div')
+//     json.forEach(image => {
+//       const h2 = document.createElement('h2')
+//       h2.innerHTML = `<h2>${image}</h2>`
+//       main.appendChild(h2)
+//     })
+//   }
